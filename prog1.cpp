b9159d178cfe0573faefc98c06ae7c56864cb43b
Ryan Honeyager <ryan@honeyager.info>
2017-07-21 14:58:33 -0400

    Switched to mesh_bounds for an easy fix.

--- prog1.cpp
+++ prog1.cpp
@@ -92,7 +92,7 @@ int main(int argc, char** argv) {
 		const double den_ice_g_cm3 = 0.9167;
 		struct analysis_data {
 			double contouredVol_cm3, contouredMass_g, discretized_volume_cm3;
-			double actual_bounds[6], mesh_bounds[6], actual_bound_volume_cm3;
+			double actual_bounds[6], mesh_bounds[6], actual_bound_volume_cm3, mesh_bound_volume_cm3;
 			double vf_contoured_boxed, resolution_mm, aeff_um;
 			size_t numIceLatticeSites, numLatticeSites;
 			std::string sIn, sOut, sFileId;
@@ -102,7 +102,7 @@ int main(int argc, char** argv) {
 					"actual_bounds[y_max](mm),actual_bounds[z_min](mm),actual_bounds[z_max](mm),"
 					"mesh_bounds[x_min](mm),mesh_bounds[x_max](mm),mesh_bounds[y_min](mm),"
 					"mesh_bounds[y_max](mm),mesh_bounds[z_min](mm),mesh_bounds[z_max](mm),"
-					"actual_bound_volume_cm3,vf_contoured_boxed,resolution_mm,aeff_um,"
+					"mesh_bound_volume_cm3,vf_contoured_boxed,resolution_mm,aeff_um,"
 					"numLatticeSites,numIceLatticeSites" << endl;
 			}
 			void write(std::ostream &out) {
@@ -111,7 +111,7 @@ int main(int argc, char** argv) {
 					<< actual_bounds[3] << "," << actual_bounds[4] << "," << actual_bounds[5] << ","
 					<< mesh_bounds[0] << "," << mesh_bounds[1] << "," << mesh_bounds[2] << ","
 					<< mesh_bounds[3] << "," << mesh_bounds[4] << "," << mesh_bounds[5] << ","
-					<< actual_bound_volume_cm3 << "," << vf_contoured_boxed << "," << resolution_mm << ","
+					<< mesh_bound_volume_cm3 << "," << vf_contoured_boxed << "," << resolution_mm << ","
 					<< aeff_um << "," << numLatticeSites << "," << numIceLatticeSites << endl;
 			}
 		};
@@ -188,7 +188,11 @@ int main(int argc, char** argv) {
 					* (data.actual_bounds[1] - data.actual_bounds[0])
 					* (data.actual_bounds[3] - data.actual_bounds[2])
 					* (data.actual_bounds[5] - data.actual_bounds[4]);
-				data.vf_contoured_boxed = data.contouredVol_cm3 / data.actual_bound_volume_cm3;
+				data.mesh_bound_volume_cm3 = std::pow(1. / 10., 3.)
+					* (data.mesh_bounds[1] - data.mesh_bounds[0])
+					* (data.mesh_bounds[3] - data.mesh_bounds[2])
+					* (data.mesh_bounds[5] - data.mesh_bounds[4]);
+				data.vf_contoured_boxed = data.contouredVol_cm3 / data.mesh_bound_volume_cm3;
 
 				double spacing[3]; // desired volume spacing
 				spacing[0] = resolution_mm;
@@ -202,15 +206,15 @@ int main(int argc, char** argv) {
 				int dim[3];
 				for (int i = 0; i < 3; i++)
 				{
-					dim[i] = static_cast<int>(ceil((data.actual_bounds[i * 2 + 1] - data.actual_bounds[i * 2]) / spacing[i]));
+					dim[i] = static_cast<int>(ceil((data.mesh_bounds[i * 2 + 1] - data.mesh_bounds[i * 2]) / spacing[i]));
 				}
 				whiteImage->SetDimensions(dim);
 				whiteImage->SetExtent(0, dim[0] - 1, 0, dim[1] - 1, 0, dim[2] - 1);
 
 				double origin[3];
-				origin[0] = data.actual_bounds[0] + spacing[0] / 2;
-				origin[1] = data.actual_bounds[2] + spacing[1] / 2;
-				origin[2] = data.actual_bounds[4] + spacing[2] / 2;
+				origin[0] = data.mesh_bounds[0] + spacing[0] / 2;
+				origin[1] = data.mesh_bounds[2] + spacing[1] / 2;
+				origin[2] = data.mesh_bounds[4] + spacing[2] / 2;
 				whiteImage->SetOrigin(origin);
 
 				whiteImage->AllocateScalars(VTK_UNSIGNED_CHAR, 1);
@@ -245,14 +249,7 @@ int main(int argc, char** argv) {
 
 				// Determine the number of filled points on the grid
 				vtkSmartPointer<vtkImageData> imgd = imgstenc->GetOutput();
-				//int* dims = imgd->GetDimensions();
-				// int dims[3]; // can't do this
 				data.numIceLatticeSites = 0;
-				//std::cout << "Dims: " << " x: " << dims[0] << " y: " << dims[1] << " z: " << dims[2] << std::endl;
-
-				//std::cout << "Number of points: " << imgd->GetNumberOfPoints() << std::endl;
-				//std::cout << "Number of cells: " << imgd->GetNumberOfCells() << std::endl;
-
 				for (int z = 0; z < dim[2]; z++)
 				{
 					for (int y = 0; y < dim[1]; y++)
